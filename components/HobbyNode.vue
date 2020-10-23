69cb987a9a22db2de99a04ce610a53802b0bc40b
KindMaple <angelattack9759@gmail.com>
2020-10-23 18:07:24 +0900

    ADD: 関連する趣味をハイパーリンクでつなげた

---
+++ components/HobbyNode.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card relative-node">
+    <nuxt-link :to="'/graph/' + node.id"
+      >関連の趣味 {{ node.title }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["node"],
+};
+</script>
+<style scoped>
+.relative-node{
+    background-color:#F18805
+}
+</style>
+
