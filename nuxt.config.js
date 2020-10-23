629702af156d579b905aaffc3ac1e48ef8b754f8
mei <mei28aquarius@gmail.com>
2020-10-23 17:26:05 +0900

    ADD: markdown func

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,10 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
       '~/plugins/articlesData',
+    {
+      src: '@/plugins/vue-mavon-editor',
+      srr: false
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
