140c0fc6509ce8dd49687a54f68c2f712b4ffe8a
Katsuya SUZUKI <syssec-diginnos>
2020-11-11 13:00:10 +0900

    ADD: firebaseに接続した
    Co-authored-by: Kurorie <kurochacha0914@icloud.com>
    Co-authored-by: niwabarubossa <niwabarubossa@yahoo.co.jp>
    Co-authored-by: mei <mei28aquarius@gmail.com>
    Co-authored-by: Hotsukai <kaihotsu.kotaro@gmail.com>
    Co-authored-by: Katsuya <angelattack9759@gmail.com>
    Co-authored-by: Nanaka <nanaka0012@yahoo.co.jp>

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '~/plugins/hobbiesData',
+    '~/plugins/firebase',
     {
       src: '@/plugins/vue-mavon-editor',
       srr: false
