9db2ce28c51c84e7fda1f2d64db3621ade5b6e4e
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-12-03 21:09:35 +0900

    update: グラフ描画範囲をウィンドウサイズに Co-authored-by: Kurorie <kurochacha0914@icloud.com> Co-authored-by: niwabarubossa <niwabarubossa@yahoo.co.jp> Co-authored-by: mei <mei28aquarius@gmail.com> Co-authored-by: Hotsukai <kaihotsu.kotaro@gmail.com> Co-authored-by: Katsuya <angelattack9759@gmail.com> Co-authored-by: Nanaka <nanaka0012@yahoo.co.jp>

--- components/GraphSvg.vue
+++ components/GraphSvg.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
-    <svg viewbox="0 0 1000 1000" width="1000" height="1000">
-      <circle @click="linkToList" r="150" cx="500" cy="500" ></circle>
+    <svg viewbox="0 0 1000 1000" :width="width" :height="height">
+      <circle @click="linkToList" :r="r" :cx="x" :cy="y" ></circle>
       <text x="500" y="500" text-anchor = "middle" dominant-baseline = "central"
          style="font-size:20px; fill: #513e35;">
-         <tspan x="500" y="490">{{ name }}</tspan>
-         <tspan x="500" y="510">の沼を見る？</tspan>
-        
-        <tbreak />
-        
+         <tspan :x="x" :y="y - 10">{{ name }}</tspan>
+         <tspan :x="x" :y="y + 10">の沼を見る？?</tspan>
       </text>
     </svg>
   </div>
@@ -20,21 +17,38 @@ export default {
   data() {
     return {
       active: false,
-        r: 80,
-        x: 200,
-        y: 100,
-    };
+      width: window.innerWidth,
+      height: window.innerHeight,
+      // 円のサイズ指定をcircle.r的な書き方にしたいんだけどやり方分からなかったやつ↓
+      r: 100,
+      x: this.width,
+      y: this.height,
+    }
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize)
+  },
+  mounted() {
+    console.log("******:")
+    console.log(this.width)
+    console.log("******:")
+    this.x = this.width/2
+    this.y = this.height/2
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
   },
   methods: {
     linkToList() {
       this.$router.push('/'+this.targetNode.id+'/list')
+    },
+    handleResize: function() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.x = this.width/2
+      this.y = this.height/2
     }
   },
-  mounted() {
-    console.log("******:")
-    console.log(this.name)
-    console.log("******:")
-  }
 }
 </script>
 
