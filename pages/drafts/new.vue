dcbd92c0ce245ddac31011ce3ac76fc64252ceef
niwabarubossa <niwabarubossa@yahoo.co.jp>
2020-12-11 22:33:19 +0900

    UPDATE:不要なコメントアウト削除

--- pages/drafts/new.vue
+++ pages/drafts/new.vue
@@ -70,12 +70,10 @@ export default {
         //新規登録
         console.log("existing tag is null")
         let documentRefId = await this.$createTag(this.tags)
-        console.log("documentRefid .." + documentRefId)
         article["tags"] = [documentRefId]
         
       }else{
         article["tags"] = [existingTag.id];
-        console.log("article tags is...." + article.tags);
           // FIXME: 入力フォームから取得したタグ名を元にタグIDを取得して配列に格納する
           // 仮データとしてサッカーのタグを選択している
           // tags: ["4qqOzg7eEx2vsBIcjTTq"],
