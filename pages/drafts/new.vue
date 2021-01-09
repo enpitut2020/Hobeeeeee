4534a8c17c91f47b70ae529e59f7a1128c2f334b
mei <mei28aquarius@gmail.com>
2021-01-09 14:03:36 +0900

    ADD: タグの複数登録機能をつけたよ!! なんと登録されている記事も一つだけ！すごい！
    Co-authored-by: mei <mei28aquarius@gmail.com>
    Co-authored-by: Hotsukai <kaihotsu.kotaro@gmail.com>
    Co-authored-by: Nanaka <nanaka0012@yahoo.co.jp>
    
    Revert "ADD: タグの複数登録機能をつけたよ!!"
    
    This reverts commit 96c0cbef906158e05f2013ec39bb8fc4c192ff5f.

--- pages/drafts/new.vue
+++ pages/drafts/new.vue
@@ -2,31 +2,17 @@
   <div>
     <div class="section">
       <input v-model="title" class="input" placeholder="タイトル" />
-      <div
-        v-for="(searchText, index) in searchTexts"
-        :key="'serchText-' + index"
-      >
-        <input
-          v-model="searchTexts[index]"
-          class="input"
-          placeholder="タグ"
-          name="yourarea"
-          autocomplete="on"
-          list="suggestList"
-        />
+      <div v-for="(searchText, index) in searchTexts" :key="'serchText-' + index">
+        <input v-model="searchTexts[index]" class="input" placeholder="タグ" name="yourarea" autocomplete="on"
+          list="suggestList" />
       </div>
       <button @click="addTagSuggestBox()">+</button>
       <button @click="deleteTagSuggestBox()">-</button>
       <datalist id="suggestList">
-        <option v-for="n in tagSuggestions" :key="n">{{ n }}</option>
+        <option v-for="(n,index) in tagSuggestions" :key="n+index">{{ n }}</option>
       </datalist>
       <input v-model="author" class="input" placeholder="書いた人" />
-      <mavon-editor
-        v-model="content"
-        :toolbars="markdownOption"
-        language="ja"
-        placeholder="記事を書いてね！"
-      />
+      <mavon-editor v-model="content" :toolbars="markdownOption" language="ja" placeholder="記事を書いてね！" />
       <button type="button" class="button is-success" @click="submit">
         投稿する
       </button>
@@ -35,116 +21,110 @@
 </template>
 
 <script>
-import "mavon-editor/dist/css/index.css";
+  import "mavon-editor/dist/css/index.css";
 
-export default {
-  async asyncData({ $getSuggestions }) {
-    let data = await $getSuggestions();
-    return { tagSuggestions: data.tagSuggestions };
-  },
-  data() {
-    return {
-      title: "",
-      content: "",
-      tags: [],
-      innerSearchText: "",
-      author: "",
+  export default {
+    async asyncData({ $getSuggestions }) {
+      let data = await $getSuggestions();
+      return { tagSuggestions: data.tagSuggestions };
+    },
+    data() {
+      return {
+        title: "",
+        content: "",
+        tags: [],
+        innerSearchText: "",
+        author: "",
         searchTexts: [""],
-      markdownOption: {
-        bold: true,
-        italic: true,
-        header: true,
-        underline: true,
-        strikethrough: true,
-        mark: true,
-        superscript: true,
-        subscript: true,
-        quote: true,
-        ol: true,
-        ul: true,
-        link: true,
-        imagelink: true,
-        code: true,
-        table: true,
-        fullscreen: false,
-        readmodel: true,
-        htmlcode: true,
-        help: true,
-      },
-    };
-  },
-
-  methods: {
-    addTagSuggestBox(){
-      this.searchTexts.push("")
-    },
-
-    deleteTagSuggestBox(){
-      if(this.searchTexts.length > 1)
-      {
-        this.searchTexts.pop()
-      }
-    },
-
-    submit: async function () {
-      let timestamp = await this.$getFirebaseTimestamp();
-      let article = {
-        id: null,
-        title: this.title,
-        body: this.content,
-        author: this.author ? this.author : "ほびーさん",
-        createdAt: timestamp,
-        updatedAt: timestamp,
+        markdownOption: {
+          bold: true,
+          italic: true,
+          header: true,
+          underline: true,
+          strikethrough: true,
+          mark: true,
+          superscript: true,
+          subscript: true,
+          quote: true,
+          ol: true,
+          ul: true,
+          link: true,
+          imagelink: true,
+          code: true,
+          table: true,
+          fullscreen: false,
+          readmodel: true,
+          htmlcode: true,
+          help: true,
+        },
       };
+    },
 
-      if (
-        this.searchTexts[0] === "" ||
-        this.title === "" ||
-        this.content === "" ||
-        this.author === ""
-      ) {
-        alert("未入力の項目があります");
-        return;
-      }
+    methods: {
+      addTagSuggestBox() {
+        this.searchTexts.push("")
+      },
 
-      if (!confirm(`以下の内容で投稿しますか？\n${this.title}`)) {
-        return;
-      }
+      deleteTagSuggestBox() {
+        if (this.searchTexts.length > 1) {
+          this.searchTexts.pop()
+        }
+      },
 
-  let pagePath=""
-    await this.searchTexts.forEach(async (searchText,index) => {
-       
-    
-      let existingTag = await this.$getExistingTag(searchText);
+      submit: async function () {
+        let timestamp = await this.$getFirebaseTimestamp();
+        let article = {
+          id: null,
+          title: this.title,
+          body: this.content,
+          author: this.author ? this.author : "ほびーさん",
+          tags: [],
+          createdAt: timestamp,
+          updatedAt: timestamp,
+        };
 
-     
-//fix tagを複数。
-      if (existingTag == null) {
-        // 新規登録
-        let newTagSuggestions = this.tagSuggestions;
-        newTagSuggestions.push(searchText);
-        let documentRefId = await this.$createTag(searchText);
-        //FIX一見追加に書き換え
-        await this.$addTagSuggestions(newTagSuggestions);
-        article["tags"] = [documentRefId];
-      } else {
-        article["tags"] = [existingTag.id];
-        // tagのvolumeを増やす
-        this.$incrementArticlesCount(existingTag.id);
-      }
+        if (
+          this.searchTexts[0] === "" ||
+          this.title === "" ||
+          this.content === "" ||
+          this.author === ""
+        ) {
+          alert("未入力の項目があります");
+          return;
+        }
 
-     //これをForのそとでやらないと記事がn件登録されちゃう
-      let transitionArticleId = await this.$registerArticle(article);
-        let transitionTagId = article["tags"];
-      if(index===0){
-        pagePath=`/${transitionTagId}/${transitionArticleId}`
-      }
-      
-       });
+        if (!confirm(`以下の内容で投稿しますか？\n${this.title}`)) {
+          return;
+        }
+
+        // await this.searchTexts.forEach(async (searchText, index) => {
+        for await (let searchText of this.searchTexts) {
+          let existingTag = await this.$getExistingTag(searchText);
+          if (existingTag == null) {
+            // 新規登録
+            let newTagSuggestions = this.tagSuggestions;
+            newTagSuggestions.push(searchText);
+            let documentRefId = await this.$createTag(searchText);
+            await this.$addTagSuggestions(newTagSuggestions);
+            article.tags.push(documentRefId)
+            console.debug("tag pushed :", article.tags)
+          } else {
+            article.tags.push(existingTag.id);
+            // tagのvolumeを増やす
+            console.debug("tag pushed :", article.tags)
+            this.$incrementArticlesCount(existingTag.id);
+          }
+        };
+
+        console.debug("debug1 tags : ", article.tags)
+        console.debug("debug1.1 : ", article)
+        const transitionArticleId = await this.$registerArticle(article);
+        const transitionTagId = article.tags[0];
+        const pagePath = `/${transitionTagId}/${transitionArticleId}`
 
         alert("記事を投稿しました!");
         this.$router.push(pagePath);
+      },
     },
-  },
-};
+  };
 </script>
