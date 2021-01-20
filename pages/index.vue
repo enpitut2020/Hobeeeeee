1f3cf1b61807c07c96a57ec918ba0cbb817d0140
mei <mei28aquarius@gmail.com>
2021-01-20 16:31:50 +0900

    ADD: 見つける，書くボタンをおす

--- pages/index.vue
+++ pages/index.vue
@@ -9,19 +9,15 @@
             <img src="/title.png" alt="" />
           </div>
           <div v-if="randomId !== ''" class="columns is-centered mt-5">
-            <div class="column is-2">
-              <nuxt-link
-                :to="randomId + '/graph/'"
-                class="button is-primary is-large button_find"
-                >趣味を見つける</nuxt-link
-              >
+            <div class="column">
+              <a class="button is-primary is-large button_find" @click="$router.push(randomId+'/graph')">
+                趣味を見つける
+              </a>
             </div>
-            <div class="column is-2">
-              <nuxt-link
-                to="/drafts/new"
-                class="button is-primary is-large button_write"
-                >趣味を書く</nuxt-link
-              >
+            <div class="column">
+              <a class="button is-primary is-large button_write" @click="$router.push('/drafts/new')">
+                趣味を書く
+              </a>
             </div>
           </div>
           <div v-else>Loading...</div>
@@ -53,73 +49,73 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      hobbeeData: {},
-      randomId: "",
-      title: "",
-    };
-  },
+  export default {
+    data() {
+      return {
+        hobbeeData: {},
+        randomId: "",
+        title: "",
+      };
+    },
 
-  async created() {
-    let tag = await this.$getRandomTags(1);
-    if (tag[0]) this.randomId = tag[0].id;
-  },
-};
+    async created() {
+      let tag = await this.$getRandomTags(1);
+      if (tag[0]) this.randomId = tag[0].id;
+    },
+  };
 </script>
 
 <style>
-.hero_text {
-  font-family: impetus, sans-serif;
-  font-size: 10em;
-}
+  .hero_text {
+    font-family: impetus, sans-serif;
+    font-size: 10em;
+  }
 
-.back_left {
-  height: 100%;
-  width: 50%;
-  position: fixed;
-  right: 50%;
-  margin-right: 31%;
-  background-position: right top;
-  background-repeat: repeat;
-  background-image: url("/hero_image.png");
-  background-size: 75%;
-  display: block;
-}
+  .back_left {
+    height: 100%;
+    width: 50%;
+    position: fixed;
+    right: 50%;
+    margin-right: 31%;
+    background-position: right top;
+    background-repeat: repeat;
+    background-image: url("/hero_image.png");
+    background-size: 75%;
+    display: block;
+  }
 
-.back_right {
-  height: 100%;
-  width: 50%;
-  position: fixed;
-  left: 50%;
-  margin-left: 32%;
-  background-repeat: repeat;
-  background-image: url("/hero_image.png");
-  background-size: 75%;
-  display: block;
-}
+  .back_right {
+    height: 100%;
+    width: 50%;
+    position: fixed;
+    left: 50%;
+    margin-left: 32%;
+    background-repeat: repeat;
+    background-image: url("/hero_image.png");
+    background-size: 75%;
+    display: block;
+  }
 
-.svg-wrapper {
-  position: relative;
-  width: 100%;
-}
+  .svg-wrapper {
+    position: relative;
+    width: 100%;
+  }
 
-.svg-wrapper svg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
+  .svg-wrapper svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 
-.button_find {
-  width: 100%;
-  height: 100%;
-}
+  .button_find {
+    width: 30vw;
+    height: 100%;
+  }
 
-.button_write {
-  width: 100%;
-  height: 100%;
-}
+  .button_write {
+    width: 30vw;
+    height: 100%;
+  }
 </style>
