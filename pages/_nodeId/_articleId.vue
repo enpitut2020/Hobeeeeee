281ba7fe5a69493bcaa6e53a206f1a387f70116b
mei <mei28aquarius@gmail.com>
2021-01-20 18:18:56 +0900

    ADD: トップページに説明がきを追加
    Co-authored-by: Kurorie <kurochacha0914@icloud.com>
    Co-authored-by: niwabarubossa <niwabarubossa@yahoo.co.jp>
    Co-authored-by: mei <mei28aquarius@gmail.com>
    Co-authored-by: Hotsukai <kaihotsu.kotaro@gmail.com>
    Co-authored-by: Katsuya <angelattack9759@gmail.com>
    Co-authored-by: Nanaka <nanaka0012@yahoo.co.jp>

--- pages/_nodeId/_articleId.vue
+++ pages/_nodeId/_articleId.vue
@@ -48,7 +48,7 @@
           <div class="column">
             <input v-model="inputComment" class="input" placeholder="コメント" />
           </div>
-          <div class="level">
+          <div class="level"></div>
             <div class="column has-text-right">
               <button :disabled="isNoComment" type="button" class="button" :class="{ 'is-primary': !isNoComment }"
                 @click="submit">
--- pages/index.vue
+++ pages/index.vue
@@ -10,17 +10,23 @@
           </div>
           <div v-if="randomId !== ''" class="columns is-centered mt-5">
             <div class="column">
-              <a class="button is-primary is-large button_find" @click="$router.push(randomId+'/graph')">
-                趣味を見つける
+              <a class="button is-primary is-size-2 is-size-3-tablet is-size-4-mobile  button_find"
+                @click="$router.push(randomId+'/graph')">
+                <font-awesome-icon icon="project-diagram" />　趣味を探す
               </a>
             </div>
             <div class="column">
-              <a class="button is-primary is-large button_write" @click="$router.push('/drafts/new')">
-                趣味を書く
+              <a class="button is-primary is-size-2 is-size-3-tablet is-size-4-mobile button_write"
+                @click="$router.push('/drafts/new')">
+                　
+                <font-awesome-icon icon="pen-nib" />　趣味を書く
               </a>
             </div>
           </div>
           <div v-else>Loading...</div>
+          <nuxt-link v-scroll-to="'#explanation'" to class="button is-warning is-inverted is-outlined hero_button">
+            more
+          </nuxt-link>
         </div>
       </div>
     </section>
@@ -29,17 +35,25 @@
       <div class="container is-max-desktop">
         <div id="explanation">
           <div class="box">
-            <h1 class="title">Hobeeeeee!!</h1>
-            <p>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+            <h1 class="title">『Hobeeeeee!!』とは？</h1>
+            <!-- 
+              - 書くこと
+              1. Hobeeeeee!!とは?
+              2. 趣味を見つけるでなにできるか？
+              3. 趣味を書くで何できるか？
+             -->
+            <p>新しい趣味を見つけたい・広めたい人に向けたwebサービスなのだぁ。
             </p>
             <br />
+            <h1 class="title">― 趣味を探す ―</h1>
             <p>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
+              趣味の沼を探索することであなたにぴったりの趣味を見つけることができるのだぁ。<br>
+              趣味同士の関係はあなたの行動によってアップデートされていくのだぁ。
             </p>
             <br />
+            <h1 class="title">― 趣味を書く ―</h1>
             <p>
-              ストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+              あなたが趣味を書くことでみんなに布教することができるのだぁ。
             </p>
           </div>
         </div>
@@ -52,9 +66,7 @@
   export default {
     data() {
       return {
-        hobbeeData: {},
         randomId: "",
-        title: "",
       };
     },
 
@@ -110,12 +122,10 @@
   }
 
   .button_find {
-    width: 30vw;
-    height: 100%;
+    max-width: 70vw;
   }
 
   .button_write {
-    width: 30vw;
-    height: 100%;
+    max-width: 70%;
   }
 </style>
